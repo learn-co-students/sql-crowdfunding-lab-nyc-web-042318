20b1b18a30823b9bb23282f97996b54b046e8146
Mendy <mendywoly@gmail.com>
2018-04-29 16:41:19 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,67 @@
+INSERT INTO users (name, age) VALUES ("PERSON1", 34);
+INSERT INTO users (name, age) VALUES ("PERSON2", 54);
+INSERT INTO users (name, age) VALUES ("PERSON3", 23);
+INSERT INTO users (name, age) VALUES ("PERSON4", 54);
+INSERT INTO users (name, age) VALUES ("PERSON5", 12);
+INSERT INTO users (name, age) VALUES ("PERSON6", 87);
+INSERT INTO users (name, age) VALUES ("PERSON7", 73);
+INSERT INTO users (name, age) VALUES ("PERSON8", 25);
+INSERT INTO users (name, age) VALUES ("PERSON9", 56);
+INSERT INTO users (name, age) VALUES ("PERSON10", 45);
+INSERT INTO users (name, age) VALUES ("PERSON11", 65);
+INSERT INTO users (name, age) VALUES ("PERSON12", 23);
+INSERT INTO users (name, age) VALUES ("PERSON13", 44);
+INSERT INTO users (name, age) VALUES ("PERSON14", 66);
+INSERT INTO users (name, age) VALUES ("PERSON15", 33);
+INSERT INTO users (name, age) VALUES ("PERSON16", 39);
+INSERT INTO users (name, age) VALUES ("PERSON17", 28);
+INSERT INTO users (name, age) VALUES ("PERSON18", 48);
+INSERT INTO users (name, age) VALUES ("PERSON19", 62);
+INSERT INTO users (name, age) VALUES ("PERSON20", 91);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+  ("Project1", "School", 15000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project2", "School", 16000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project3", "School", 17000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project4", "School", 18000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project5", "Business", 19000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project6", "Business", 20000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project7", "Business", 21000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project8", "Business", 22000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project9", "Business", 23000, "4, 23, 2018", "8, 3, 2018"),
+  ("Project10", "Business", 24000, "4, 23, 2018", "8, 3, 2018")
+;
+
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+  (500, 1, 3),
+  (500, 2, 4),
+  (500, 3, 5),
+  (500, 4, 3),
+  (500, 7, 7),
+  (500, 6, 8),
+  (500, 5, 9),
+  (500, 20, 10),
+  (500, 19, 10),
+  (500, 18, 9),
+  (500, 17, 8),
+  (500, 16, 7),
+  (500, 15, 6),
+  (500, 14, 5),
+  (500, 13, 4),
+  (500, 12, 3),
+  (500, 11, 2),
+  (500, 10, 1),
+  (500, 9, 2),
+  (500, 8, 3),
+  (500, 7, 4),
+  (500, 6, 5),
+  (500, 5, 6),
+  (500, 4, 7),
+  (500, 3, 8),
+  (500, 2, 9),
+  (500, 1, 10),
+  (500, 10, 9),
+  (500, 8, 8),
+  (500, 4, 7)
+;
